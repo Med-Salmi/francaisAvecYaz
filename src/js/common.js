826d718f5fe4d2e6cbcf9af6ee95f2917f5e9989
Mohammed Salmi <medsalmipro@gmail.com>
2025-08-16 16:12:31 +0100

    Handle buttons pressing effect on mobile

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,18 @@
 import { initHeader } from "../components/shared/header/header";
 import { initHeaderMenu } from "../components/shared/header/header";
 
+function enableMobileActiveButtons() {
+  const buttons = document.querySelectorAll(".button");
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("touchstart", () => btn.classList.add("pressed"));
+    btn.addEventListener("touchend", () => btn.classList.remove("pressed"));
+    btn.addEventListener("touchcancel", () => btn.classList.remove("pressed"));
+  });
+}
+
 export function initCommon() {
   initHeader();
   initHeaderMenu();
+  enableMobileActiveButtons(); // initialize button pressed effect
 }
